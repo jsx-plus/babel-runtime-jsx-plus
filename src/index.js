ff45aa89fab69c9aede159ead8c3d231516d5cac
卓凌 <zhuoling.lcl@alibaba-inc.com>
2019-07-18 17:22:01 +0800

    feat: support createList for list

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import $slot from './Slot';
 import createJSXMemo from './createJSXMemo';
+import createList from './createList';
 import classnames from './classnames';
 
 export {
+  createList,
   createJSXMemo,
   classnames,
   $slot,
