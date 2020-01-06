5dd953b7ebd9d4a0522be4771faf7836a3262723
卓凌 <zhuoling.lcl@alibaba-inc.com>
2020-01-06 15:42:21 +0800

    fix: runtime slots

--- src/createCondition.js
+++ src/createCondition.js
@@ -1,5 +1,3 @@
-const isArray = Array.isArray;
-
 /**
  * Create condition helper.
  * Structure:
@@ -25,7 +23,7 @@ const isArray = Array.isArray;
  * @return {null}
  */
 export default function createCondition(conditions) {
-  if (isArray(conditions)) {
+  if (Array.isArray(conditions)) {
     for (let i = 0, l = conditions.length; i < l; i++) {
       const [getCondition, getRender] = conditions[i];
       if (getCondition()) return getRender();
